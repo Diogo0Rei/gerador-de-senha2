733718b2d41275b05d3ca3c6e4f36445c359a7ed
Diogo0Rei <diogo.soares.filho@escola.pr.gov.br>
2026-09-10 09:53:55 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,15 @@
+const numeroSenha = document.querySelector('.parametro-senha__texto');
+let tamanhoSenha = 12;
+numeroSenha.textContent = tamanhoSenha;
 
+const letrasMaiusculas = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+const letrasMinisculas = 'abcdefghijklmnopqrstuvwxyz';
+const numeros = '0123456789';
+const simbolos = '!@#$%&*?_+';
+const botoes = document.querySelectorAll('.parametro-senha__botao');
+const campoSenha = document.querySelector('#campo-senha');
+const checkbox = document.querySelectorAll('.chechbox');
+const forcaSenha = document.querySelector('.forca');
+
+botoes[0].onclick = diminuiTamanho;
+botoes[1].onclick = aumentaTamanho;
